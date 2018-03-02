5cf69707386413d91436b8cf39e380592c830509
Ramsay Jones <ramsay@ramsayjones.plus.com>
2018-03-02 02:56:05 +0000

    parse-options: remove the unused parse_opt_commits() function
    Commit fcfba37337 ('ref-filter: make "--contains <id>" less chatty if
    <id> is invalid', 2018-02-23), removed the last use of the callback
    function parse_opt_commits(). Remove this function declaration and
    definition, since it is now dead code.
    
    Signed-off-by: Ramsay Jones <ramsay@ramsayjones.plus.com>
    Reviewed-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- parse-options-cb.c
+++ parse-options-cb.c
@@ -78,22 +78,6 @@ int parse_opt_verbosity_cb(const struct option *opt, const char *arg,
 	return 0;
 }
 
-int parse_opt_commits(const struct option *opt, const char *arg, int unset)
-{
-	struct object_id oid;
-	struct commit *commit;
-
-	if (!arg)
-		return -1;
-	if (get_oid(arg, &oid))
-		return error("malformed object name %s", arg);
-	commit = lookup_commit_reference(&oid);
-	if (!commit)
-		return error("no such commit %s", arg);
-	commit_list_insert(commit, opt->value);
-	return 0;
-}
-
 int parse_opt_object_name(const struct option *opt, const char *arg, int unset)
 {
 	struct object_id oid;
